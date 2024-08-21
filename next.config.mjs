961433f0d98e6dc4c0b462b07bc4423c8c8e1686
Ri-Hong <riri.hong@gmail.com>
2024-08-21 18:18:31 -0400

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     output: 'export',
     basePath: '/git-green-squares',
-    // assetPrefix: '/',
     images: {
       unoptimized: true,  // Required for static exports
     },
